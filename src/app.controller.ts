2dfd9a36c7cebfa1deb5ef4f5c1b8db3233584bf
misostack <misostack@gmail.com>
2020-10-14 04:57:39 +0200

    Init

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get()
+  @Render('pages/index')
+  root() {
+    return { title: 'Title', message: 'Hello world!' };
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // MVC
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
